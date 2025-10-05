10ed0e8a0062b2502025b947c9aeaac6104edc00
Arwa Ayman <arwaayman790@gmail.com>
2025-10-05 21:44:59 +0300

    Added Home Screen

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // removes the debug banner
+      title: 'Empty Page',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const EmptyPage(),
+    );
+  }
+}
+
+class EmptyPage extends StatelessWidget {
+  const EmptyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Empty Page'),
+      ),
+      body: const Center(
+        child: Text(
+          'This is an empty page',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
